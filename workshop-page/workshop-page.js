e0affb7860e837cf221dbe7b701909b0fe265ff2
‘AMAYA <aamaya547@gmail.com>
2022-05-24 11:19:02 -0700

    workshops appending to workshops page

--- workshop-page/workshop-page.js
+++ workshop-page/workshop-page.js
@@ -1,19 +1,32 @@
-import { checkAuth, getWorkshops, logout } from '../fetch-utils.js';
-import { renderWorkshop } from '../render-utils.js';
+import { checkAuth, getWorkshops, createParticipant, logout } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
-const workshopsEl = document.querySelector('workshops-container');
+const workshopsEl = document.querySelector('.workshops-container');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
 async function displayWorkshops() {
-    const main = document.querySelector('main');
-    main.textContent = '';
+    // const main = document.querySelector('main');
+    workshopsEl.textContent = '';
     const data = await getWorkshops();
-    console.log(data);
+    // console.log(data);
+    for (let workshop of data) {
+        const div = document.createElement('div');
+        div.classList.add('workshop');
+
+        const h3 = document.createElement('h3');
+        h3.textContent = workshop.name;
+
+        const div2 = document.createElement('div');
+        div2.classList.add('bunnies');
+
+        div.append(h3, div2);
+        workshopsEl.append(div);
+    }
+    // console.log(workshopsEl);
 }
 displayWorkshops();

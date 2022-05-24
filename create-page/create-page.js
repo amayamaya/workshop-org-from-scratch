d23856b6f50af2621aa033e46a8ec815c74491d3
‘AMAYA <aamaya547@gmail.com>
2022-05-24 12:35:14 -0700

    options pulling from SB after adding onLoad function

--- create-page/create-page.js
+++ create-page/create-page.js
@@ -1,11 +1,18 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, getWorkshops, logout } from '../fetch-utils.js';
 import { renderOption } from '../render-utils.js';
 
 const participantForm = document.querySelector('.participant-form');
-const workshopSelct = document.getElementById('workshops')
+const workshopSelect = document.getElementById('workshops');
 const logoutButton = document.getElementById('logout');
 
-
+async function onLoad() {
+    const workshops = await getWorkshops();
+    for (let workshop of workshops) {
+        const workshopOption = renderOption(workshop);
+        workshopSelect.append(workshopOption);
+    }
+}
+onLoad();
 
 participantForm.addEventListener('submit', async (e) => {
     e.preventDefault();
